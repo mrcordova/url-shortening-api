e9e2205aec072b0bd97c8ec4494c5f07b82cbdcc
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-07-31 20:45:39 -0700

    refactor create list code

--- js/script.js
+++ js/script.js
@@ -1,13 +1,4 @@
-// - Shorten any valid URL done
-// - See a list of their shortened links, even after refreshing the browser
-// - Copy the shortened link to their clipboard in a single click done
-// - Receive an error message when the `form` is submitted if: done
-//   - The `input` field is empty
-// Notes
-// Long URLs should be URL-encoded. You can not include a longUrl in the request that has &, ?, #, or other reserved parameters without first encoding it.
-// Long URLs should not contain spaces: any longUrl with spaces will be rejected. All spaces should be either percent encoded %20 or plus encoded +. Note that tabs, newlines and trailing spaces are all indications of errors. Please remember to strip leading and trailing whitespace from any user input before shortening.
 const URL = "https://tinyurl.com/api-create.php?url=";
-// const API_ENDPOINT = "https%3A%2F%2Fgoogle.com%2F";
 const linkInput = document.getElementById("url");
 const shortenBtn = document.querySelector(".url-input-container>button");
 const errorSpan = document.querySelector(".error-empty");
@@ -17,22 +8,23 @@ window.addEventListener("load", () => {
   if (localStorage.getItem("links")) {
     const items = JSON.parse(localStorage.getItem("links"));
     for (const item of items) {
-      links.insertAdjacentHTML(
-        "beforeend",
-        `<li>
-              <p>${item.url}</p>
-              <p>${item.shortenUrl}</p>
-              <button>Copy</button>
-              </li>`
-      );
-
-      links.lastElementChild.lastElementChild.addEventListener(
-        "click",
-        copyUrl
-      );
+      createLi(item.url, item.shortenUrl);
     }
   }
 });
+
+function createLi(url, shortenUrl) {
+  links.insertAdjacentHTML(
+    "beforeend",
+    `<li>
+              <p>${url}</p>
+              <p>${shortenUrl}</p>
+              <button>Copy</button>
+              </li>`
+  );
+
+  links.lastElementChild.lastElementChild.addEventListener("click", copyUrl);
+}
 
 function populateLinks() {
   if (!localStorage.getItem("links")) {
@@ -67,17 +59,9 @@ shortenBtn.addEventListener("click", async (e) => {
   );
   if (linkInput.checkValidity()) {
     const shortenUrl = await getUrl(linkInput.value);
-    links.insertAdjacentHTML(
-      "beforeend",
-      `<li>
-        <p>${linkInput.value}</p>
-        <p>${shortenUrl}</p>
-        <button>Copy</button>
-        </li>`
-    );
-
-    links.lastElementChild.lastElementChild.addEventListener("click", copyUrl);
+    createLi(linkInput.value, shortenUrl);
     populateLinks();
+    linkInput.value = "";
   }
 });
 
